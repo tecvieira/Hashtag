d0d4686d3d62cc01f20787bc7609bda67f6fa1d5
Robson Vieira <tecvander.vieira@gmail.com>
2026-07-06 21:50:42 -0300

    inicio da aula 1

---
+++ aula1_automaçãoTarefa/codigo.py
@@ -0,0 +1,19 @@
+import pyautogui
+import time
+
+# bibbliotecas = pacotes de códigos prontos que podemos usar no nosso programa
+#Passo a passo do seu programa
+#passo1 entrar no sistema da empresa
+#passo2 fazer login
+#passo3 abrir base de dados
+#passo4 cadastrar um produto
+#passo5 repetir o passo 4 até acabar a lista de produtos
+pyautogui.PAUSE = 0.5
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+# fazer uma pausa de 2 segundos para o navegador abrir
+time.sleep(3)
+pyautogui.click(x=647, y=447)
+pyautogui.write("https://www.sistemaempresa.com")
+pyautogui.press("enter")
